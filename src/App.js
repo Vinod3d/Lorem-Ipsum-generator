bf3075fd3505a2fa055431cf3795abdaf58780fd
Vinod Chandra <98041742+Vinod3d@users.noreply.github.com>
2023-07-28 21:28:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import "./styles.css";
+import data from "./data";
+import { useState } from "react";
+import { nanoid } from "nanoid";
+
+export default function App() {
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    setText(data.slice(0, amount));
+  };
+  return (
+    <section className="section-center">
+      <h4>tired of boring lorem ipsum</h4>
+
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min="1"
+          step="1"
+          max="8"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+
+        <button className="btn" type="submit">
+          Generate
+        </button>
+      </form>
+
+      <article className="lorem-text">
+        {text.map((item) => {
+          return <p key={nanoid()}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
+}
